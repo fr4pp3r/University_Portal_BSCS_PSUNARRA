8bf6c871217bd59e64a990133ef20ddd9dccd236
fr4pp3r <nathanielbetita83@gmail.com>
2024-10-23 12:34:28 +0800

    idk

--- FinalOutputOOP/AdminForm.Designer.vb
+++ FinalOutputOOP/AdminForm.Designer.vb
@@ -68,7 +68,7 @@ Partial Class AdminForm
         ' 
         ' AdminForm
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.FromArgb(CByte(255), CByte(164), CByte(67))
         ClientSize = New Size(1024, 576)
